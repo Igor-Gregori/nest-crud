cd70b6bcf5af9ae8d04b1ca01a0807ed9c2f8042
Igor Gregori <igsilvacarlos@gmail.com>
2021-06-19 13:53:10 -0300

    adding some settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { UsersModule } from './users/users.module';
+
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      'sua string de conexão',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.mongodb_uri),
   ],
   controllers: [],
   providers: [],
