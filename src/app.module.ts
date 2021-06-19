e6a8bfade392019fbf8d58fbe16d3e08a00b5025
Igor Gregori <igsilvacarlos@gmail.com>
2021-06-19 14:10:03 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.mongodb_uri),
   ],
   controllers: [],
